0338d9707f38e13e9ca9d4d4ef008354c4374db5
RENO <reno@Air-de-RENO.lan>
2026-01-20 23:38:57 +0100

    ajustement css et UI

---
+++ pokeApp/urls.py
@@ -0,0 +1,31 @@
+"""
+URL configuration for pokeApp project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from pokedex import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('add_team/<int:pokemon_id>/', views.add_to_team, name='add_team'),
+    path('combat/', views.combat, name='combat'),
+    path('attack/', views.attack_turn, name='attack_turn'),
+    path('switch/<int:index>/', views.switch_pokemon, name='switch_pokemon'),
+    path('reset_combat/', views.reset_combat, name='reset_combat'),
+    path('clear_team/', views.clear_team, name='clear_team'),
+    path('remove_member/<int:member_index>/', views.remove_team_member, name='remove_team_member'),
+]
